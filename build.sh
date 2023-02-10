6072c04c99c2d884022979e7db87c9b693b3791e
Joseph Lou <jdlou@princeton.edu>
2023-02-10 12:32:25 -0500

    Add build and start command scripts

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Build command for a deployment
+
+# Upgrade pip to the latest version
+python -m pip install --upgrade pip
+
+# Install the dependencies
+PIPENV_VENV_IN_PROJECT=1
+python -m pip install pipenv wheel
+pipenv install --deploy --ignore-pipfile --verbose
+
+# Run database migrations
+# flask db upgrade
