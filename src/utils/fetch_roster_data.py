1a8f9ae5d6d063ef43d058ba3eaed82c417a9c3c
Joseph Lou <jdlou@princeton.edu>
2023-03-09 01:12:27 -0500

    Add roster processing

--- src/utils/fetch_roster_data.py
+++ src/utils/fetch_roster_data.py
@@ -4,6 +4,9 @@ Fetches the full team roster from the given spreadsheet link.
 
 # =============================================================================
 
+import re
+from functools import partial
+
 import google.auth.exceptions
 import gspread
 
@@ -12,6 +15,9 @@ import db
 # =============================================================================
 
 ROSTER_WORKSHEET_NAME = "FULL ROSTER"
+
+# Note: If any of these values are changed, they must also be changed in
+# `validate_roster()`.
 REQUIRED_COLUMNS = [
     "name",
     "email",
@@ -22,6 +28,20 @@ OPTIONAL_COLUMNS = [
     "team code",
     "fighting weight class",
 ]
+
+POSSIBLE_ROLES = [
+    "COACH",
+    "ATHLETE",
+    "SPECTATOR",
+]
+POSSIBLE_WEIGHT_CLASSES = [
+    "light",
+    "middle",
+    "heavy",
+    "alternate",
+]
+
+TEAM_CODE_PATTERN = re.compile(r"(P|((Men|Women)'s ))[ABC]\d+")
 
 # =============================================================================
 
@@ -84,50 +104,76 @@ def fetch(url):
     No validation is done beyond required and optional columns.
 
     Returns:
-        Union[Tuple[str, None], Tuple[None, List[Dict]]]:
-            A tuple of an error message, or a list of each row in the
-            sheet, represented as a dict with the lowercase keys of all
-            the required and optional columns.
+        Union[Tuple[str, None, None], Tuple[None, List, Dict]]:
+            A tuple of: an error message, a list of log messages, and
+            the full roster in the format:
+                'schools': [list of school names]
+                'users': list of:
+                    'name': name
+                    'email': email
+                    'role': role
+                    'school': school name
+                'teams': list of:
+                    'school': school name
+                    'code': team code
+                    'light': (optional) email
+                    'middle': (optional) email
+                    'heavy': (optional) email
+                    'alternates': list of emails
     """
+
+    def _fetch_error(msg):
+        return msg, None, None
 
     service_account_info = db.global_state.get_service_account_info()
     if service_account_info is None:
-        return "No service account in database", None
+        return _fetch_error("No service account in database")
 
     error_msg, client = get_service_account_client(service_account_info)
     if error_msg is not None:
-        return f"Invalid service account credentials: {error_msg}", None
+        return _fetch_error(
+            f"Invalid service account credentials: {error_msg}"
+        )
 
     spreadsheet_id = url_to_id(url)
     if spreadsheet_id is None:
-        return "Invalid spreadsheet link", None
+        return _fetch_error("Invalid spreadsheet link")
 
+    # get spreadsheet and worksheet
     try:
         spreadsheet = client.open_by_key(spreadsheet_id)
     except gspread.SpreadsheetNotFound:
-        return "Spreadsheet not found", None
+        return _fetch_error("Spreadsheet not found")
     except gspread.exceptions.APIError as ex:
         error = ex.response.json()["error"]
         if error["code"] == 403 and error["status"] == "PERMISSION_DENIED":
-            return (
+            return _fetch_error(
                 "Service account does not have permission to open the "
-                "spreadsheet",
-                None,
+                "spreadsheet"
             )
         if error["code"] == 404 and error["status"] == "NOT_FOUND":
             # not sure why this isn't a `SpreadsheetNotFound` error?
-            return "Spreadsheet not found", None
+            return _fetch_error("Spreadsheet not found")
         raise
     try:
         worksheet = spreadsheet.worksheet(ROSTER_WORKSHEET_NAME)
     except gspread.WorksheetNotFound:
-        return f"Roster worksheet {ROSTER_WORKSHEET_NAME!r} not found", None
+        return _fetch_error(
+            f"Roster worksheet {ROSTER_WORKSHEET_NAME!r} not found"
+        )
 
     worksheet_values = worksheet.get_values()
     if len(worksheet_values) == 0:
-        return f"Empty roster worksheet {ROSTER_WORKSHEET_NAME!r}", None
+        return _fetch_error(
+            f"Empty roster worksheet {ROSTER_WORKSHEET_NAME!r}"
+        )
 
     header_row, *rows = worksheet_values
+
+    if len(rows) == 0:
+        return _fetch_error(
+            f"Empty roster worksheet {ROSTER_WORKSHEET_NAME!r}"
+        )
 
     # first row should (uniquely) contain all the headers
     # maps: header -> column index
@@ -148,51 +194,280 @@ def fetch(url):
             missing.append(f'"{header}"')
     if len(non_unique) > 0:
         non_unique_str = ", ".join(non_unique)
-        return (
-            (
-                f"Invalid roster worksheet {ROSTER_WORKSHEET_NAME!r}: "
-                f"repeated headers {non_unique_str}"
-            ),
-            None,
+        return _fetch_error(
+            f"Invalid roster worksheet {ROSTER_WORKSHEET_NAME!r}: "
+            f"repeated headers {non_unique_str}"
         )
     if len(missing) > 0:
         missing_str = ", ".join(missing)
-        return (
-            (
-                f"Invalid roster worksheet {ROSTER_WORKSHEET_NAME!r}: "
-                f"missing headers {missing_str}"
-            ),
-            None,
+        return _fetch_error(
+            f"Invalid roster worksheet {ROSTER_WORKSHEET_NAME!r}: "
+            f"missing headers {missing_str}"
         )
 
-    roster = []
-    for row in rows:
-        athlete_data = {}
-        missing_required = False
-        for header in REQUIRED_COLUMNS:
-            value = row[header_col_index[header]].strip()
-            if value == "":
-                missing_required = True
-                break
-            athlete_data[header] = value
-        if missing_required:
-            # skip: assume invalid
-            continue
-        for header in OPTIONAL_COLUMNS:
-            value = row[header_col_index[header]].strip()
-            if value == "":
-                value = None
-            athlete_data[header] = value
-        roster.append(athlete_data)
+    def yield_row_values():
+        for i, row in enumerate(rows):
+            row_values = {"row_num": i + 2, "missing_required": []}
+            for header in REQUIRED_COLUMNS:
+                value = row[header_col_index[header]].strip()
+                if value == "":
+                    row_values["missing_required"].append(f'"{header}"')
+                else:
+                    row_values[header] = value
+            for header in OPTIONAL_COLUMNS:
+                value = row[header_col_index[header]].strip()
+                if value == "":
+                    value = None
+                row_values[header] = value
+            yield row_values
 
-    if len(roster) == 0:
-        return "Empty roster (no valid rows found)", None
+    logs, roster = process_roster(yield_row_values)
+    for key in ("users", "teams", "schools"):
+        if len(roster[key]) == 0:
+            return _fetch_error(f"Empty roster (no {key} found)")
 
     spreadsheet_url_with_worksheet = id_to_url(spreadsheet_id, worksheet.id)
     success = db.global_state.set_last_fetched_spreadsheet_url(
         spreadsheet_url_with_worksheet
     )
     if not success:
-        return "Unknown database error; please try again later", None
+        return _fetch_error("Unknown database error; please try again later")
 
-    return None, roster
+    return None, logs, roster
+
+
+# =============================================================================
+
+
+def _list_of_items(items):
+    if len(items) == 0:
+        return None
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return " and ".join(items)
+    return ", ".join(items[:-1]) + ", and " + items[-1]
+
+
+def process_roster(row_generator):
+    """Processes the rows yielded by the given generator.
+
+    Assumes emails uniquely identify each user, but doesn't do any
+    rigorous email checking. That is, emails are taken as they were
+    entered in the sheet, although they are technically case insensitive
+    and dot insensitive and parts after a "+" character can be ignored.
+
+    Returns:
+        Tuple[List, Dict]:
+            A list of log messages and the roster in the format:
+                'schools': [list of school names]
+                'users': list of:
+                    'name': name
+                    'email': email
+                    'role': role
+                    'school': school name
+                'teams': list of:
+                    'school': school name
+                    'code': team code
+                    'light': (optional) email
+                    'middle': (optional) email
+                    'heavy': (optional) email
+                    'alternates': list of emails
+    """
+
+    logs = []
+
+    def _log(level, msg, row_num):
+        logs.append({"level": level, "row_num": row_num, "message": msg})
+
+    # maps: school name -> true
+    schools = {}
+    # maps: email -> user
+    users = {}
+    # maps: (school, code) -> team
+    teams = {}
+
+    def _add_user(email, name, role, school):
+        users[email] = {
+            "name": name,
+            "email": email,
+            "role": role,
+            "school": school,
+        }
+
+    def _add_team(school, team_code):
+        team = {
+            "school": school,
+            "code": team_code,
+            "light": None,
+            "middle": None,
+            "heavy": None,
+            "alternates": [],
+        }
+        teams[school, team_code] = team
+        return team
+
+    def _is_user_in_team(team, email):
+        for key in ("light", "middle", "heavy"):
+            if team[key] == email:
+                return True
+        if email in team["alternates"]:
+            return True
+        return False
+
+    def _add_user_to_team(team, email, weight_class=None):
+        if weight_class is None:
+            # poomsae team: just add wherever fits
+            for key in ("light", "middle", "heavy"):
+                if team[key] is None:
+                    team[key] = email
+                    return
+            team["alternates"].append(email)
+        else:
+            # sparring team: add for proper weight class
+            # assume the weight class is not taken yet
+            if weight_class == "alternate":
+                team["alternates"].append(email)
+            else:
+                team[weight_class] = email
+
+    for row_data in row_generator():
+        row_num = row_data["row_num"]
+        _log_info = partial(_log, "INFO", row_num=row_num)
+        _log_error = partial(_log, "ERROR", row_num=row_num)
+        _log_warning = partial(_log, "WARNING", row_num=row_num)
+
+        missing_required = row_data["missing_required"]
+        if len(missing_required) > 0:
+            missing_str = ", ".join(missing_required)
+            _log_error(f"Missing required values: {missing_str}")
+            continue
+
+        name = row_data["name"]
+        email = row_data["email"]
+        role = row_data["role"].upper()
+        school = row_data["school"]
+
+        name_email = f"{name} <{email}>"
+
+        if role not in POSSIBLE_ROLES:
+            _log_error(f'Invalid role "{role}" (skipped)')
+            continue
+        is_athlete = role == "ATHLETE"
+
+        if email in users:
+            # validate that the user is the same
+            seen_before = users[email]
+            different = []
+            if name != seen_before["name"]:
+                different.append(f"name {name!r}")
+            if role != seen_before["role"]:
+                different.append(f'role "{role}"')
+            if school != seen_before["school"]:
+                different.append(f"school {school!r}")
+            if len(different) > 0:
+                _log_error(
+                    f'Repeated email "{email}" with different '
+                    f"{_list_of_items(different)} (skipped)"
+                )
+                continue
+
+            # can skip non-athletes
+            if not is_athlete:
+                # note: not checking the team data columns
+                _log_warning(
+                    f'No need to repeat a "{role}" user row (skipped)'
+                )
+                continue
+
+        # simple case: non-athletes
+        if not is_athlete:
+            if school not in schools:
+                schools[school] = True
+                _log_info(f"Added school: {school}")
+            _add_user(email, name, role, school)
+            _log_info(f"Added {role.lower()}: {name_email} ({school})")
+            # shouldn't have any team data
+            has_data = []
+            for key in OPTIONAL_COLUMNS:
+                if row_data[key] is not None:
+                    has_data.append(key)
+            if len(has_data) > 0:
+                _log_warning(
+                    f"Unnecessary data for {_list_of_items(has_data)} "
+                    '(not "ATHLETE" role)'
+                )
+            continue
+
+        # find out which team it is
+        team_code = row_data["team code"]
+        if team_code is None:
+            _log_error("Missing team code (skipped)")
+            continue
+        if TEAM_CODE_PATTERN.fullmatch(team_code) is None:
+            _log_error(f"Invalid team code {team_code!r} (skipped)")
+            continue
+        weight_class = row_data["fighting weight class"]
+        if weight_class is not None:
+            weight_class = weight_class.lower()
+        # assume poomsae teams start with "P" and sparring teams do not
+        is_sparring_team = team_code[0].upper() != "P"
+        if is_sparring_team:
+            if weight_class is None:
+                _log_error(
+                    "Missing fighting weight class for sparring team (skipped)"
+                )
+                continue
+            if weight_class not in POSSIBLE_WEIGHT_CLASSES:
+                _log_error(f"Invalid weight class {weight_class!r} (skipped)")
+                continue
+        elif not is_sparring_team and weight_class is not None:
+            _log_warning("Unnecessary fighting weight class for poomsae team")
+            weight_class = None
+
+        school_team = (school, team_code)
+        school_team_str = " ".join(school_team)
+        created_team = False
+        if school_team in teams:
+            team = teams[school_team]
+            if _is_user_in_team(team, email):
+                _log_warning(
+                    f"Athlete already in team {school_team_str!r} (skipped)"
+                )
+                continue
+            # make sure there's an available spot for sparring team
+            if (
+                is_sparring_team
+                and weight_class != "alternate"
+                and team[weight_class] is not None
+            ):
+                _log_error(
+                    f"Team {school_team_str!r} already has a "
+                    f"{weight_class}-weight athlete (skipped)"
+                )
+                continue
+        else:
+            team = _add_team(school, team_code)
+            created_team = True
+
+        if school not in schools:
+            schools[school] = True
+            _log_info(f"Added school: {school}")
+        if email not in users:
+            _add_user(email, name, role, school)
+            _log_info(f"Added athlete: {name_email} ({school})")
+        if created_team:
+            _log_info(f"Added team: {school_team_str}")
+
+        _add_user_to_team(team, email, weight_class)
+        msg = f"Added athlete {name_email} to team: {school_team_str}"
+        if is_sparring_team:
+            msg += f" ({weight_class.capitalize()})"
+        _log_info(msg)
+
+    roster = {
+        "schools": list(schools.keys()),
+        "users": list(users.values()),
+        "teams": list(teams.values()),
+    }
+    return logs, roster
--- src/views/notifications.py
+++ src/views/notifications.py
@@ -5,6 +5,9 @@ team info, and sending notifications.
 
 # =============================================================================
 
+import json
+from pathlib import Path
+
 from flask import flash, request
 
 import db
@@ -15,23 +18,30 @@ from utils.routes import AppRoutes, _render
 
 app = AppRoutes()
 
+STATIC_FOLDER = Path(__file__).parent / ".." / "static"
+FETCH_ROSTER_LOGS_FILE = (STATIC_FOLDER / "fetch_roster_logs.json").resolve()
+
 # =============================================================================
+
+
+def get_last_fetched_time_str():
+    last_fetched_dt = db.global_state.get_last_fetched_time()
+    if last_fetched_dt is None:
+        return None
+    return last_fetched_dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
 @app.route("/notifications", methods=["GET"])
 def notifications():
     spreadsheet_url = db.global_state.get_last_fetched_spreadsheet_url()
-
-    last_fetched_time = None
-    last_fetched_dt = db.global_state.get_last_fetched_time()
-    if last_fetched_dt is not None:
-        last_fetched_time = last_fetched_dt.strftime("%Y-%m-%d %H:%M:%S")
-
+    last_fetched_time = get_last_fetched_time_str()
+    has_fetch_logs = FETCH_ROSTER_LOGS_FILE.exists()
     return _render(
         "notifications/index.jinja",
         roster_worksheet_name=fetch_roster_data.ROSTER_WORKSHEET_NAME,
         spreadsheet_url=spreadsheet_url,
         last_fetched_time=last_fetched_time,
+        has_fetch_logs=has_fetch_logs,
     )
 
 
@@ -44,13 +54,41 @@ def fetch_roster():
         return {"success": False, "reason": 'Missing "url" key in JSON data'}
     url = request_args["url"]
 
-    error_msg, roster = fetch_roster_data.fetch(url)
+    error_msg, logs, roster = fetch_roster_data.fetch(url)
     if error_msg is not None:
         return {"success": False, "reason": error_msg}
 
-    # TODO: validate the roster
-    # TODO: process the roster before saving it
+    # TODO: save the roster
+
+    # save the logs in a file
+    full_logs = {
+        "time_fetched": get_last_fetched_time_str(),
+        "logs": logs,
+    }
+    FETCH_ROSTER_LOGS_FILE.write_text(
+        json.dumps(full_logs, indent=2), encoding="utf-8"
+    )
 
     # use flash so that the url and last fetched time are updated
     flash("Success", "info")
     return {"success": True, "roster": roster}
+
+
+@app.route("/notifications/fetch_roster_logs", methods=["GET"])
+def fetch_roster_logs():
+    time_fetched = None
+    logs = None
+    if FETCH_ROSTER_LOGS_FILE.exists():
+        try:
+            full_logs = json.loads(FETCH_ROSTER_LOGS_FILE.read_bytes())
+            time_fetched = full_logs["time_fetched"]
+            logs = full_logs["logs"]
+        except (json.decoder.JSONDecodeError, KeyError):
+            # delete the file; it's faulty somehow
+            FETCH_ROSTER_LOGS_FILE.unlink()
+    return _render(
+        "notifications/fetch_roster_logs.jinja",
+        fetch_logs_filename=FETCH_ROSTER_LOGS_FILE.name,
+        time_fetched=time_fetched,
+        logs=logs,
+    )
