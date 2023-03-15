9fa5f8366233a7dc0df3484c7e9cd41e1df2e7f2
Joseph Lou <jdlou@princeton.edu>
2023-03-15 11:10:07 -0400

    Add input to fetch match info
    Currently only parses the match numbers and displays errors accordingly.

--- src/views/notifications.py
+++ src/views/notifications.py
@@ -8,7 +8,7 @@ team info, and sending notifications.
 import json
 from pathlib import Path
 
-from flask import flash
+from flask import flash, request
 
 import db
 from utils import fetch_tms
@@ -170,3 +170,137 @@ def view_full_roster():
 @app.route("/notifications/full_roster/raw", methods=["GET"])
 def view_full_roster_raw():
     return db.roster.get_full_roster(as_json=True)
+
+
+def _parse_matches_query(matches_query):
+    """Handwritten parser to parse a match list str, with spaces or
+    commas separating match groups, and with dashes representing match
+    ranges.
+
+    The parser is pretty forgiving; it allows numbers starting with 0s
+    and has no restrictions on the value ranges. However, the end of
+    ranges must be at least as large as the start of the range. Also,
+    there is a cutoff on the number of matches that can be specified.
+    """
+    MAX_NUM_MATCHES = 50
+
+    def _parse_error(msg, index=None):
+        if index is not None:
+            msg = f"Position {index+1}: {msg}"
+        return msg, None
+
+    # insertion-order collection of match numbers
+    match_numbers = {}
+    # a buffer of the last seen match number
+    last_num = None
+    # index of the seen dash
+    saw_dash = None
+    # the digits of the current number
+    current_number = []
+
+    def _add_number(i, c=None):
+        nonlocal last_num, saw_dash
+
+        if len(current_number) == 0:
+            return None
+
+        num_start_i = i - len(current_number)
+        num = int("".join(current_number))
+        current_number.clear()
+
+        if saw_dash is not None:
+            # end a range
+            if num < last_num:
+                return (
+                    f"Position {num_start_i}: "
+                    "Range end is smaller than range start"
+                )
+            if num == last_num:
+                match_numbers[last_num] = True
+            else:
+                for x in range(last_num, num + 1):
+                    match_numbers[x] = True
+            last_num = None
+            saw_dash = None
+        else:
+            # single match number
+            if last_num is not None:
+                match_numbers[last_num] = True
+            last_num = num
+
+        if len(match_numbers) > MAX_NUM_MATCHES:
+            return f"Too many matches specified (max {MAX_NUM_MATCHES})"
+
+        return None
+
+    for i, c in enumerate(matches_query):
+        if c.isspace() or c in (",", "-"):
+            # end of a number
+            error_msg = _add_number(i, c)
+            if error_msg is not None:
+                return _parse_error(error_msg)
+            if c == ",":
+                # explicitly start a new group (dashes cannot go across
+                # commas)
+                if saw_dash is not None:
+                    return _parse_error(
+                        f"Position {saw_dash+1}: Dash without end number"
+                    )
+                if last_num is not None:
+                    match_numbers[last_num] = True
+                last_num = None
+            elif c == "-":
+                if last_num is None:
+                    return _parse_error(
+                        f"Position {i+1}: Dash without a start number"
+                    )
+                if saw_dash is not None:
+                    return _parse_error(f"Position {i+1}: invalid dash")
+                saw_dash = i
+            continue
+        if c.isdigit():
+            current_number.append(c)
+            continue
+        return _parse_error(f"Position {i+1}: unknown character: {c}")
+    error_msg = _add_number(len(matches_query))
+    if error_msg is not None:
+        return _parse_error(error_msg)
+
+    if saw_dash is not None:
+        return _parse_error(f"Position {saw_dash+1}: Dash without end number")
+
+    if last_num is not None:
+        match_numbers[last_num] = True
+    if len(match_numbers) > MAX_NUM_MATCHES:
+        return _parse_error(
+            f"Too many matches specified (max {MAX_NUM_MATCHES})"
+        )
+
+    return None, list(match_numbers.keys())
+
+
+@app.route("/notifications/matches_info", methods=["GET"])
+def fetch_matches_info():
+    def _error(msg):
+        return {"success": False, "reason": msg}
+
+    matches_query = request.args.get("matches", None)
+    if matches_query is None:
+        return _error("No matches given.")
+    matches_query = matches_query.strip()
+
+    print(" ", "Fetching matches for query:", matches_query)
+
+    error_msg, match_numbers = _parse_matches_query(matches_query)
+    if error_msg is not None:
+        print(" ", "Error parsing match query:", error_msg)
+        return _error(error_msg)
+    if len(match_numbers) == 0:
+        print(" ", "Error: No match numbers given")
+        return _error("No match numbers given.")
+    match_numbers.sort()
+    print(" ", "Parsed match numbers:", match_numbers)
+
+    # TODO: fetch match info
+
+    return {"success": True, "match_numbers": match_numbers}
