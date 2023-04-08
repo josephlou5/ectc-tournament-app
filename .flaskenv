8b4b5880b0289de00cf552249f0a54afe7491b59
Joseph Lou <jdlou@princeton.edu>
2023-04-08 18:17:35 -0400

    Make it easier to start a development server

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,1 @@
-# Run `flask run` to start a development server at http://localhost:5000/
 FLASK_APP=src/app.py
--- src/runserver.py
+++ src/runserver.py
@@ -1,12 +1,24 @@
 """
-Runs the server in debug mode.
+Runs a development server in debug mode.
+
+For HTTPS, you must have a `cert.pem` and `key.pem` file in the root
+directory.
+
+You also must have the environment variables `GOOGLE_CLIENT_ID` and
+`GOOGLE_CLIENT_SECRET` set for authentication through Google.
 """
 
 # =============================================================================
 
-from app import app
+import os
+
+# Set `FLASK_DEBUG` so that the app is initialized in debug mode
+# Expects any string
+os.environ["FLASK_DEBUG"] = "True"
+
+from app import app  # pylint: disable=wrong-import-position
 
 # =============================================================================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
