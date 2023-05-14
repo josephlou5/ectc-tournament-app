5c494c47c19318f71f1d8ac9fc9c4665429e78f1
Joseph Lou <jdlou@princeton.edu>
2023-05-13 22:15:31 -0400

    Add required checks for running a local dev server

--- src/runserver.py
+++ src/runserver.py
@@ -11,14 +11,42 @@ You also must have the environment variables `GOOGLE_CLIENT_ID` and
 # =============================================================================
 
 import os
-
-# Set `FLASK_DEBUG` so that the app is initialized in debug mode
-# Expects any string
-os.environ["FLASK_DEBUG"] = "True"
-
-from app import app  # pylint: disable=wrong-import-position
+from pathlib import Path
 
 # =============================================================================
 
+PORT = 5000
+
+# =============================================================================
+
+
+def main():
+    invalid = False
+
+    for key in ("GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"):
+        if not os.getenv(key):
+            invalid = True
+            print("Environment variable not set:", key)
+
+    root = (Path(__file__).parent / "..").resolve()
+    for file in ("cert.pem", "key.pem"):
+        path = (root / file).resolve()
+        if not path.exists():
+            invalid = True
+            print("Missing file:", path.relative_to(root))
+
+    if invalid:
+        return
+
+    # Set `FLASK_DEBUG` so that the app is initialized in debug mode
+    if not os.getenv("FLASK_DEBUG"):
+        # Expects any string
+        os.environ["FLASK_DEBUG"] = "True"
+
+    from app import app  # pylint: disable=import-outside-toplevel
+
+    app.run(host="0.0.0.0", port=PORT, ssl_context=("cert.pem", "key.pem"))
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
+    main()
